539867a3b1e2bb27feffb7a3ff771170450823d9
Manuel Bertele <manuel.bertele2003@gmail.com>
2021-04-26 19:53:54 +0200

    Removed unnecessary brackets
    Signed-off-by: Manuel Bertele <manuel.bertele2003@gmail.com>

--- src/xmemtools.c
+++ src/xmemtools.c
@@ -40,10 +40,9 @@ void *xmemdup(void *pntr, size_t bytes) {
 
 char *xstrdiff(char *s1, char *s2) {
 
-    for (;*s1 == *s2; ++s1, ++s2) {
+    for (;*s1 == *s2; ++s1, ++s2)
         if (*s1 == '\0')
             return s1;
-    }
     
     return s1;
 
