e648e95228cc5c113d32931cd2c69fdc457cc7b1
Manuel Bertele <manuel.bertele2003@gmail.com>
2021-06-03 19:07:35 +0200

    Removed tabs for spaces
    Signed-off-by: Manuel Bertele <manuel.bertele2003@gmail.com>

--- src/xmemtools.c
+++ src/xmemtools.c
@@ -6,28 +6,28 @@
 
 void *xmalloc(size_t bytes) {
 
-	void *temp = malloc(bytes);
-	assert(temp != NULL);
-	return (temp);
+    void *temp = malloc(bytes);
+    assert(temp != NULL);
+    return (temp);
 }
 
 void *xcalloc(size_t nelem, size_t bytes) {
 
-	void *temp = calloc(nelem, bytes);
-	assert(temp != NULL);
-	return (temp);
+    void *temp = calloc(nelem, bytes);
+    assert(temp != NULL);
+    return (temp);
 }
 
 void *xrealloc(void *pntr, size_t bytes) {
 
-	void *temp = realloc(pntr, bytes);
-	assert(temp != NULL);
-	return (temp);
+    void *temp = realloc(pntr, bytes);
+    assert(temp != NULL);
+    return (temp);
 }
 
 void xfree(void *pntr) {
 
-	free(pntr);
+    free(pntr);
 }
 
 void *xmemdup(void *pntr, size_t bytes) {
